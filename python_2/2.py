45f1315bf621f30ec8ce06f5915242e9a69312ea
nstartsev <100710617+nstartsev@users.noreply.github.com>
2022-07-15 14:30:06 +0300

    Create 2.py

---
+++ python_2/2.py
@@ -0,0 +1,10 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = input()
+    print(s[::-1])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
