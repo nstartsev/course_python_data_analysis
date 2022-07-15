dec57cc7c7112e57d73ff3b65cfb7cac0a7d0490
nstartsev <100710617+nstartsev@users.noreply.github.com>
2022-07-15 14:31:31 +0300

    Create 6.py

---
+++ python_2/6.py
@@ -0,0 +1,15 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = input()
+    s1 = s.replace(' ', '')
+    s2 = s1.replace('-', '')
+    s3 = s2.replace('(', '')
+    s4 = s3.replace(')', '')
+    print(s4)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
