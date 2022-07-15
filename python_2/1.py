c1c973cab6ef2424c0d52cfe54ba9c4472d4dac4
nstartsev <100710617+nstartsev@users.noreply.github.com>
2022-07-15 14:29:36 +0300

    Create 1.py

---
+++ python_2/1.py
@@ -0,0 +1,10 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = input()
+    print(len(s))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
