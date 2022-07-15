dea9a883d162addb6d003430e8af1037b1e75d47
nstartsev <100710617+nstartsev@users.noreply.github.com>
2022-07-15 14:30:52 +0300

    Create 4.py

---
+++ python_2/4.py
@@ -0,0 +1,15 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    data = []
+    f = input()
+    s = f.split()
+    data.append(int(s[0]))
+    data.append(int(s[1]))
+    print(data[1],data[0])
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
