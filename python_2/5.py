711fad127ac2f42b29a0c4b981be21dfde6439ee
nstartsev <100710617+nstartsev@users.noreply.github.com>
2022-07-15 14:31:11 +0300

    Create 5.py

---
+++ python_2/5.py
@@ -0,0 +1,12 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = input()
+    a  = s.find('@')
+    print(s[:a])
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
